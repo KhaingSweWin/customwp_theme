7469e2eaf8adb8fdeb6fee5946105e55af9e3c9e
Khaing Swe Win <khaingswewin@hostmyanmar.net>
2022-01-17 15:47:15 +0630

    custom theme

---
+++ mytheme/footer.php
@@ -0,0 +1,8 @@
+		<footer>
+		<div id="footer">&copy; <?php date('Y'); ?> <?php bloginfo('name'); ?> Theme is copyrighted to Vincy.</div>
+		</footer>
+		</div>
+	</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ mytheme/sidebar.php
@@ -0,0 +1,6 @@
+<div id="sidebar">
+<h2>Categories</h2>
+<ul >
+<?php wp_list_cats(); ?>
+</ul>
+</div>
